93e8afd0e4a13eab19b67a6173790968d1c66633
MertCetin <mert.cetnn@gmail.com>
2022-06-28 17:23:06 -0400

    true false confirm codes add

--- Develop-1/script.js
+++ Develop-1/script.js
@@ -1,59 +1,61 @@
+// creting arrays
+var lowerCase = ["abcdefghijklmnopqrstuvwxyz"];
+var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var numeric = ["0123456789"];
+var specialc = ["!#$%&'()*+,-./:;<=>?@[]^_`{|}~"];
+// Assignment code here
+function generatePassword() {
+  var password = "";
+  var pwdCases = "";
+1  //creates prombts
+  var passwordLength = prompt("How many character would you like to use in your password? (min:8 ,max:128!)")
+ 
+  if (passwordLength < 8) {
+    window.alert("Password can not be less than 7 characters.");
+    return;
+  };
+  if (passwordLength > 128) 
+    {window.alert("Password can not be more than 128 characters.")
+  return;
+};
+
+
+  
+  //true and false confirms
+  var lowerCaseConfirm = confirm("Would you like to use LowerCase characters?");
+  if (lowerCaseConfirm) {
+    pwdCases += lowerCase;
+  };
+ var upperCaseConfirm= confirm("Would you like to use UpperCase characters?");
+ if (upperCaseConfirm ){
+  pwdCases=+upperCase;
+ };
+var numericConfirm= confirm("Would you like to see more numbers in your password?");
+if(numericConfirm){
+  pwdCases+=numeric;
+};
+var specialcConfirm= confirm("would you like to add some special characters?")
+if (specialcConfirm){
+  pwdCases=specialc;
+}
+
+
+
+
+}
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword() {
-  // // Arrays of Possible Character Choices
-  var lowerCase = ["abcdefghijklmnopqrstuvwxyz"];
-  var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-  var numeric = ["0123456789"];
-  var specialc = ["!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"];
-  var characters=""
-
-
-  while (characters === "") {
-    if (window.confirm("Would you like to confirm lowercase characters?")) {
-      characters += lowerCase
-    }
-    if (window.confirm("Would you like to confirm uppercase characters?")) {
-      characters += upperCase
-    }
-    if (window.confirm("Would you like to add number?")) {
-      characters += numeric
-    
-    }
-    if (confirm("Would you like to use special characters?")) {
-      characters += specialc
-    }
-  
-
-  var password = ""
-
-  while (numberofcharacters < 8 || numberofcharacters > 128) {
-    numberofcharacters = Number(prompt("Choose  Password  8 to 128"))
-  }
-  for (let i = 0; i < generatePassword; i++) {
-    var index = Math.floor(Math.random() * generatePassword.length)
-    password +=  characters[i]
-
-
-  }
-  return password
-
-}
-}
-
-
 // Write password to the #password input
 function writePassword() {
-
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
