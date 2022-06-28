c1e50052c851cef5469d422083ec42edbe7ee1a8
MertCetin <mert.cetnn@gmail.com>
2022-06-28 14:41:43 -0400

    first path deleted new loops adding

--- Develop-1/script.js
+++ Develop-1/script.js
@@ -3,42 +3,40 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   // // Arrays of Possible Character Choices
-  characters = ""
-  var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-  var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  var specialc = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "/", "]", "^", "_", "`", "{", "|", "}", "~"];
+
+  let values
+   lowerCase = ["abcdefghijklmnopqrstuvwxyz"],
+   upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"],
+   numeric = ["0123456789"],
+   specialc = ["!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"];
+   let password ="";
 
 
-  while (characters === "") {
-    if (window.confirm("Would you like to confirm lowercase characters?")) {
-      characters += lowerCase
-    }
-    if (window.confirm("Would you like to confirm uppercase characters?")) {
-      characters += upperCase
-    }
-    if (window.confirm("Would you like to add number?")) {
-      characters += numeric
-    }
-    if (confirm("Would you like to use special characters?")) {
-      characters += specialc
-    }
+   for (var i =0; i<=complexity; i++){
+    password=password + values.chartAt(Math.floor(Math.random()*Math.floor(value.length-1)));
+   }
+  
+
+
+
+}
+
+
+
+  var password = ""
+  var numberofcharacters = 0
+  while (numberofcharacters < 8 || numberofcharacters > 128) {
+    numberofcharacters = Number(prompt("Choose  Password  8 to 128"))
   }
+  for (let i = 0; i < numberofcharacters; i++) {
+    var index = Math.floor(Math.random() * characters.length)
+    password = + characters[index]
 
 
-    var password = ""
-    var numberofcharacters = 0
-    while (numberofcharacters < 8 || numberofcharacters > 128) {
-      numberofcharacters = Number(prompt("Choose  Password  8 to 128"))
-    }
-    for (let i = 0; i < numberofcharacters; i++) {
-      var index = Math.floor(Math.random() * characters.length)
-      password = + characters[index]
-    
   }
   return password
 
-}
+
 
 
 
