2ff46f4321ee41618bd14ca78ce25b3431ccb400
MertCetin <mert.cetnn@gmail.com>
2022-06-28 10:12:35 -0400

    variebles and loops adjusted

--- Develop-1/script.js
+++ Develop-1/script.js
@@ -1,13 +1,5 @@
-function charset() {
-  var numberA = (numberA => 8, numberA++ <= 128);
-  var lowerCase = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
-  var upperCase = ("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
-  var numeric = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
-  var specialch = ("!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "/", "]", "^", "_", "`", "{", "|", "}", "~");
 
-}
- var charset=  promptnumberA,confirmUpperCase,confirmLowerCase,confirmNumeric,confirmSpecialCh;
-function promptnumberA() {
+function promptnumbers() {
   var passwordText = prompt("Please enter character amount.(Min: 8 , Max: 128)");  
 }
 function confirmLowerCase() {
@@ -24,17 +16,41 @@ function confirmSpecialCh () {
   var passwordText = window.confirm ( "Would you like to use special characters?")
   
 }
-promptnumberA();
-confirmLowerCase();
-confirmUpperCase();
-confirmNumeric();
-confirmSpecialCh();
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
   function generatePassword() {
+
+    var numbers = (numbers => 8, numbers++ <= 128);
+  var lowerCase = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+  var upperCase = ("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
+  var numeric = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+  var specialch = ("!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "/", "]", "^", "_", "`", "{", "|", "}", "~");
+  var characters ("");
+
+while(characters === ""){
+  function promptnumbers() {
+    var passwordText = prompt("Please enter character amount.(Min: 8 , Max: 128)");
+
+  }
+  function confirmLowerCase() {
+    var passwordText = window.confirm("Would you like to confirm lowercase characters?");
   
+  }
+  function confirmUpperCase(){
+    var passwordText = window.confirm("Would you like to confirm uppercase characters?");
+  }
+  function confirmNumeric (){
+    var passwordText = window.confirm("Would you like to add number?");   
+  }
+  function confirmSpecialCh () {
+    var passwordText = window.confirm ( "Would you like to use special characters?")
+    
+  }
+
+
+
 }
 
  
