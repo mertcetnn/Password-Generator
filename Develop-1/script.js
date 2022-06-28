24a72d706bd3eef1428baaf4fd8852cc7d699115
MertCetin <mert.cetnn@gmail.com>
2022-06-28 18:17:56 -0400

    all prompts ready , working on display text

--- Develop-1/script.js
+++ Develop-1/script.js
@@ -3,46 +3,58 @@ var lowerCase = ["abcdefghijklmnopqrstuvwxyz"];
 var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
 var numeric = ["0123456789"];
 var specialc = ["!#$%&'()*+,-./:;<=>?@[]^_`{|}~"];
+
+
 // Assignment code here
 function generatePassword() {
   var password = "";
   var pwdCases = "";
 1  //creates prombts
-  var passwordLength = prompt("How many character would you like to use in your password? (min:8 ,max:128!)")
- 
+ let passwordLength = prompt("How many character would you like to use in your password? (min:8 ,max:128!)")
+
   if (passwordLength < 8) {
     window.alert("Password can not be less than 7 characters.");
-    return;
   };
+
   if (passwordLength > 128) 
     {window.alert("Password can not be more than 128 characters.")
-  return;
 };
+
+
 
 
   
   //true and false confirms
-  var lowerCaseConfirm = confirm("Would you like to use LowerCase characters?");
+  let lowerCaseConfirm = confirm("Would you like to use LowerCase characters?");
   if (lowerCaseConfirm) {
     pwdCases += lowerCase;
-  };
- var upperCaseConfirm= confirm("Would you like to use UpperCase characters?");
+  } ;
+let upperCaseConfirm= confirm("Would you like to use UpperCase characters?");
  if (upperCaseConfirm ){
   pwdCases=+upperCase;
- };
-var numericConfirm= confirm("Would you like to see more numbers in your password?");
+ }else{
+  }
+  
+  ;
+let numericConfirm= confirm("Would you like to see more numbers in your password?");
 if(numericConfirm){
   pwdCases+=numeric;
-};
-var specialcConfirm= confirm("would you like to add some special characters?")
+}else{
+  }
+  ;
+let specialcConfirm= confirm("would you like to add some special characters?")
 if (specialcConfirm){
   pwdCases=specialc;
+}else{
+  } 
+
+  
+return password
 }
 
 
 
 
-}
 
 
 // Get references to the #generate element
