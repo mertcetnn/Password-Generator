c3720c84ce8af5815be18b638ae2206a8b48f637
MertCetin <mert.cetnn@gmail.com>
2022-06-30 00:24:41 -0400

    last touches

--- Develop-1/script.js
+++ Develop-1/script.js
@@ -1,58 +1,62 @@
 
 
 // Assignment code here
-function generatePassword(password) {
-// creting arrays
-var lowerCase = "abcdefghijklmnopqrstuvwxyz"
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var numeric = "0123456789"
-var specialc = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
-var character=""
+function generatePassword() {
+  // creting arrays
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz"
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  var numeric = "0123456789"
+  var specialc = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
+  var character = ""
 
-while(character === ""){ 
-  //true and false confirms
-  if (confirm("Would you like to use LowerCase characters?"))
-   character+= lowerCase;}
-if (!lowerCase){alert("You must chose at least one")};
+  while (character === "" )  {
+    //true and false confirms
+    if (confirm("Would you like to use LowerCase characters?")) {
+      character += lowerCase
+    }
+
+    if (confirm("Would you like to use UpperCase characters?")) {
+      character += upperCase
+    }
+
+
+
+    if (confirm("Would you like to see more numbers in your password?")) {
+      character += numeric
+    }
+
+
+
+    if (confirm("would you like to add some special characters?")) {
+      character += specialc
+    }
+  }
  
-  if (confirm("Would you like to use UpperCase characters?")){
-    character += upperCase; };
-  if(!upperCase){ alert("You must chose at least one")}
 
-
-
- if(confirm("Would you like to see more numbers in your password?"));{
-    character += numeric;};
-    if(!numericConfirm){ alert("You must chose at least one")}
-
-
-    
- if( confirm("would you like to add some special characters?")) {
-    character += specialc;};
-    if (!specialcConfirm){alert("You must chose at least one")}
-  
-  
+  //adjsuting character number amount of password
+ 
+  var numberofcharacters = 0
+  while (numberofcharacters < 8 || numberofcharacters > 128) {
+    numberofcharacters = Number(parseInt(prompt("number of characters? Choose between 8 to 128!")))
   }
 
-  var passwordS= ""
-  var numberofcharacters=0
+  // using one character each time
+  var passwordS = ""
 
-  while(numberofcharacters<8 || numberofcharacters>128){
-    numberofcharacters=Number(parseInt(prompt("number of characters? Choose between 8 to 128!")))
-  }
-  for (let i=0; i<numberofcharacters; i++){
-    var index =Math.floor(Math.random()*character.length)
-    passwordS += character[index]
+  for (let i = 0; i < numberofcharacters; i++) {
+    passwordS += Math.floor(Math.random() * character.length)
+    passwordS += character
 
-  return password;
+    return password
 
   }
-
+}
 
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
